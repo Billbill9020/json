1f01e691b3f37e605e158892be18a0f09db2f448
Billbill9020 <thuvien93@gmail.com>
2021-08-20 16:03:50 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,163 @@
+export default DATA = [
+
+    {
+        "id": "1097000400338789_4039980892707377",
+        "updated_time": "2021-04-16T02:32:25+0000",
+        "from": {
+            "name": "C\u00f4 B\u00e9 Ch\u00e2n G\u00e0",
+            "id": "100038785303818"
+        },
+        "message": "từ hoàng mai đi vĩnh phúc, ship 20k, liên hệ: 0987234823",
+        "type": "photo"
+    },
+    {
+        "id": "1097000400338789_4040639765974823",
+        "updated_time": "2021-04-16T02:26:03+0000",
+        "from": {
+            "name": "Linh Tr\u1ecbnh",
+            "id": "100007184921178"
+        },
+        "message": "20 ng\u00f5 2 t\u00e2n tri\u1ec1u \u0111i Ho\u00e0ng qu\u1ed1c vi\u1ec7t\nKh\u00f4ng \u1ee9ng\nShip 40k\nLh 0986621413",
+        "type": "status"
+    },
+    {
+        "id": "1097000400338789_4040643742641092",
+        "updated_time": "2021-04-16T02:31:12+0000",
+        "from": {
+            "name": "T\u1ea5m Chi\u1ebfu M\u1edbi",
+            "id": "100003804139764"
+        },
+        "message": "C\u1ea7n 1 th\u1ee3 l\u1eafp \u0111i\u1ec1u ho\u00e0 \u1edf tam trinh công 200k",
+        "type": "status"
+    },
+    {
+        "id": "1097000400338789_4037887449583388",
+        "updated_time": "2021-04-16T02:31:01+0000",
+        "from": {
+            "name": "Th\u1ecbt B\u00f2 Kh\u00f4",
+            "id": "100004106475859"
+        },
+        "message": "Cần ship từ ngã tư sở đi long biên ship 40k. call .",
+        "type": "status"
+    },
+    {
+        "id": "1097000400338789_4040648882640578",
+        "updated_time": "2021-04-16T02:30:42+0000",
+        "from": {
+            "name": "Thanh Cao",
+            "id": "100006357582763"
+        },
+        "message": "lấy hàng ở bạch mai đi cầu giấy ,ship 40k đi luôn.call:0797005868",
+        "type": "photo"
+    },
+    {
+        "id": "1097000400338789_4039186589453474",
+        "updated_time": "2021-04-16T02:30:09+0000",
+        "from": {
+            "name": "Ho\u00e0ng Gia T\u00ednh",
+            "id": "100010280074659"
+        },
+        "message": "từ long biên đến mai động ship 40k, ai nhận liên hê 0823422922",
+        "type": "photo"
+    },
+    {
+        "id": "1097000400338789_4040647302640736",
+        "updated_time": "2021-04-16T02:29:57+0000",
+        "from": {
+            "name": "Vi \u0110\u00f4 Tr\u00ec",
+            "id": "100056363261647"
+        },
+        "message": "nhậ hàng ở phố huế đi láng, ship 30k, lh 0945398668",
+        "type": "status"
+    },
+    {
+        "id": "1097000400338789_4040631555975644",
+        "updated_time": "2021-04-16T02:29:37+0000",
+        "from": {
+            "name": "Thanh Ngoan Tran",
+            "id": "100005617481633"
+        },
+        "message": "M\u00ecnh c\u1ea7n l\u1ea5y gi\u1ea5y t\u1edd \u1edf h\u1ecdc vi\u1ec7n ngo\u1ea1i giao g\u1eedi xe n\u00e0o g\u1ea7n nh\u1ea5t v\u1ec1 TB ,b\u1ea1n n\u00e0o g\u1ea7n ko \u1ea1",
+        "type": "photo"
+    },
+    {
+        "id": "1097000400338789_4040644705974329",
+        "updated_time": "2021-04-16T02:28:40+0000",
+        "from": {
+            "name": "Thi\u1ec7n Ph\u1ea1m",
+            "id": "100030494675221"
+        },
+        "message": "có đơn từ bạch mai đi vĩnh hưng, ship 30k, đi luôn gọi 0968333888",
+        "type": "photo"
+    },
+    {
+        "id": "1097000400338789_4040643212641145",
+        "updated_time": "2021-04-16T02:28:10+0000",
+        "from": {
+            "name": "Minh",
+            "id": "100003265683224"
+        },
+        "message": "lấy hàng ở ngoại giao đoàn đi hoàng cầu call ngay 096.156.1992",
+        "type": "photo"
+    },
+    {
+        "id": "1097000400338789_4040640105974789",
+        "updated_time": "2021-04-16T02:27:15+0000",
+        "from": {
+            "name": "Nguy\u1ec5n Th\u1eafm",
+            "id": "100012205352673"
+        },
+        "message": "Sn 2 \u0111\u1ed1i di\u1ec7n c\u1ed5ng b\u00eanh vi\u00ean \u0111a khoa ph\u01b0\u01a1ng \u0111\u00f4ng \u0111i \nNh\u00e0 h\u00e0ng l\u1ea9u ho\u00e0ng minh m\u1ea1c th\u00e1i t\u1ed5 \nShip 30k\nG\u1ecdi 0986886926",
+        "type": "status"
+    },
+    {
+        "id": "1097000400338789_4040641642641302",
+        "updated_time": "2021-04-16T02:27:01+0000",
+        "from": {
+            "name": "B\u00ecnh Nguy\u1ec5n",
+            "id": "100045313035085"
+        },
+        "message": "trương định đi yên phụ ship 40 đi luôn 0986333992",
+        "type": "status"
+    },
+    {
+        "id": "1097000400338789_4037721326266667",
+        "updated_time": "2021-04-16T02:26:40+0000",
+        "from": {
+            "name": "Minh Minh",
+            "id": "100054562843757"
+        },
+        "message": "ai tiện đường nhận đơn giúp em từ hai bà trưng đi âu cơ, ship 35k ,gọi em 0389227992.",
+        "type": "photo"
+    },
+    {
+        "id": "1097000400338789_4040640325974767",
+        "updated_time": "2021-04-16T02:26:27+0000",
+        "from": {
+            "name": "Kh\u00f4 B\u00f2 Th\u1ecbt",
+            "id": "100015674983145"
+        },
+        "message": "nhận hàng đi giáp bát, lấy hàng ở vĩnh hưng LH e 0363804311.e \u1edf kh\u00e2m thi\u00ean.\n            ( Ib _zalo_sms_call)",
+        "type": "photo"
+    },
+    {
+        "id": "1097000400338789_4040639765974823",
+        "updated_time": "2021-04-16T02:26:03+0000",
+        "from": {
+            "name": "Linh Tr\u1ecbnh",
+            "id": "100007184921178"
+        },
+        "message": "20 ng\u00f5 2 t\u00e2n tri\u1ec1u \u0111i Ho\u00e0ng qu\u1ed1c vi\u1ec7t\nKh\u00f4ng \u1ee9ng\nShip 30k\nLh 0986621413",
+        "type": "status"
+    },
+    {
+        "id": "1097000400338789_4040626882642778",
+        "updated_time": "2021-04-16T02:24:39+0000",
+        "from": {
+            "name": "Tr\u1ea7n Ph\u01b0\u01a1ng Thanh",
+            "id": "100006035222211"
+        },
+        "message": "nhậ hàng ở tam trinh đi hai bà trưng ứng 500k ship 30k, gọi ngay 0968343433",
+        "type": "photo"
+    }
+]
