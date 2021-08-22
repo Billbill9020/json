5a1e7a3f4a3838feafb4295ae91aeffbd1240a89
Billbill9020 <thuvien93@gmail.com>
2021-08-22 20:06:21 +0700

    Create group2.js

---
+++ group2.js
@@ -0,0 +1,153 @@
+[
+
+    {
+        "id": "1097000400338789_4039980892707377",
+        "created_time": "2021-07-16T03:32:25+0000",
+        "from": {
+            "name": "Nguyễn Hoài",
+            "id": "100038785303818"
+        },
+        "message": "Cần từ long biên đi hoang mai, sh 30k,",
+        "type": "status"
+    },
+    {
+        "id": "1097000400338789_4040639765974823",
+        "created_time": "2021-07-16T04:26:03+0000",
+        "from": {
+            "name": "Long Nguyễn",
+            "id": "100007184921178"
+        },
+        "message": "Từ trung hoà  Đi toà hoàng cầu  Ứng 1trieu  Ship 40k  0988103316",
+        "type": "status"
+    },
+    {
+        "id": "1097000400338789_4040643742641092",
+        "created_time": "2021-07-16T03:11:12+0000",
+        "from": {
+            "name": "Nguyễn Nam",
+            "id": "100003804139764"
+        },
+        "message": "Đi từ thạch cầu đến ngô gia tự ship 30k, đi luôn 0928343342",
+        "type": "status"
+    },
+    {
+        "id": "1097000400338789_4037887449583388",
+        "created_time": "2021-07-16T01:21:01+0000",
+        "from": {
+            "name": "Hoàng",
+            "id": "100004106475859"
+        },
+        "message": "từ cầu giấy đi phạm văn đồng phí 30 liên hệ.. 0988776915",
+        "type": "status"
+    },
+    {
+        "id": "1097000400338789_4040648882640578",
+        "created_time": "2021-06-16T05:30:42+0000",
+        "from": {
+            "name": "Tiến Lê",
+            "id": "100006357582763"
+        },
+        "message": "Bạch mai đi nguyễn trãi ship 50 cần đi luôn, call 0860000325",
+        "type": "photo"
+    },
+    {
+        "id": "1097000400338789_4039186589453474",
+        "created_time": "2021-06-16T03:15:09+0000",
+        "from": {
+            "name": "Nguyễn Mạnh",
+            "id": "100010280074659"
+        },
+        "message": "Từ minh khai đi đường láng sh 30k, gọi 0823422922",
+        "type": "photo"
+    },
+    {
+        "id": "1097000400338789_4040647302640736",
+        "created_time": "2021-07-16T02:29:57+0000",
+        "from": {
+            "name": "Bảo Nam",
+            "id": "100056363261647"
+        },
+        "message": "Ngụy như con tum đi yên phụ đi luôn 60k 0901794922",
+        "type": "status"
+    },
+    {
+        "id": "1097000400338789_4040631555975644",
+        "created_time": "2021-07-16T08:21:37+0000",
+        "from": {
+            "name": "Trần Trang",
+            "id": "100005617481633"
+        },
+        "message": "nguyễn trãi thanh xuân đi ngô xuân quảng ship 100k, ai nhận gọi 098004573",
+        "type": "photo"
+    },
+    {
+        "id": "1097000400338789_4040644705974329",
+        "created_time": "2021-06-16T09:16:40+0000",
+        "from": {
+            "name": "My Nguyễn",
+            "id": "100030494675221"
+        },
+        "message": "Võ thị sáu đi văn phú - hà đông Ứng 60 ship 40k, đi luôn khách cần luôn ạ 0981 79 5995",
+        "type": "photo"
+    },
+    {
+        "id": "1097000400338789_4040643212641145",
+        "created_time": "2021-07-16T06:09:10+0000",
+        "from": {
+            "name": "Hương Lê",
+            "id": "100003265683224"
+        },
+        "message": "32 nguyễn chính đi 21 đình thôn ứng 53 ship 40 0904969808 lẩu Fa 45k/set",
+        "type": "photo"
+    },
+    {
+        "id": "1097000400338789_4040640105974789",
+        "created_time": "2021-08-16T02:06:15+0000",
+        "from": {
+            "name": "Đỗ Minh ",
+            "id": "100012205352673"
+        },
+        "message": "Kim Mã đi đường liên mạc  Ứ 360k S40k 0856133416",
+        "type": "status"
+    },
+    {
+        "id": "1097000400338789_4040641642641302",
+        "created_time": "2021-08-16T03:51:01+0000",
+        "from": {
+            "name": "Hoàng Trần",
+            "id": "100045313035085"
+        },
+        "message": "Cần ship từ dịch vọng đi trương định, ship 40 gọi 0963203571",
+        "type": "status"
+    },
+    {
+        "id": "1097000400338789_4037721326266667",
+        "created_time": "2021-08-16T02:26:40+0000",
+        "from": {
+            "name": "Hải Nguyen",
+            "id": "100054562843757"
+        },
+        "message": "Sáng mai e cần ship đi long biên call 0984139431",
+        "type": "photo"
+    },
+    {
+        "id": "1097000400338789_4040640325974767",
+        "created_time": "2021-08-16T02:52:27+0000",
+        "from": {
+            "name": "Bùi Tiến",
+            "id": "100015674983145"
+        },
+        "message": "Hoàng cầu đi cầu giấy gọi 096004573, ship 30k",
+        "type": "photo"
+    },
+    {
+        "id": "1097000400338789_4040639765974823",
+        "created_time": "2021-08-16T02:26:03+0000",
+        "from": {
+            "name": "Ngo Long",
+            "id": "100007184921178"
+        },
+        "message": "Ship phố huế đi long biên ship 40k đi luôn gọi 0380045723",
+        "type": "status"
+    }
+]
